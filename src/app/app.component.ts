ec4764c7101b54dfb5d521bc52e1df8c9d20203d
vuph31 <vuph31@gmail.com>
2019-06-20 17:22:03 +0900

    2019/06/20 Add Search Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {}
+export class AppComponent  {
+  wordSearchForm;
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+      this.wordSearchForm = this.formBuilder.group({
+      search_word:''
+    });
+    }
+  ngOnInit() {
+  }
+  onSubmit(searchData){
+     // Process checkout data here
+    console.warn('Your order has been submitted', searchData);
+  }
+}
 
