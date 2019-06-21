2c900b1208cb397e68aa40735daf6a955d955a13
vuph31 <vuph31@gmail.com>
2019-06-21 11:31:46 +0900

    Web Frame OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
+import { DOCUMENT } from '@angular/common';
 import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  animations:[ 
+    trigger('fade',
+    [ 
+      state('void', style({ opacity : 0})),
+      transition(':enter',[ animate(300)]),
+      transition(':leave',[ animate(500)]),
+    ]
+  )]
 })
 export class AppComponent  {
   wordSearchForm;
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    @Inject(DOCUMENT) document
   ) {
       this.wordSearchForm = this.formBuilder.group({
-      search_word:''
-    });
+        search_word:''
+      });
     }
   ngOnInit() {
   }
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+     /*if (window.pageYOffset > 68) {
+       let element = document.getElementById('navbar');
+       element.classList.add('sticky');
+     } else {
+      let element = document.getElementById('navbar');
+        element.classList.remove('sticky'); 
+     }*/
+  }
   onSubmit(searchData){
      // Process checkout data here
-    console.warn('Your order has been submitted', searchData);
+    alert("Chưa có dữ liệu đâu mà search");
   }
 }
 
